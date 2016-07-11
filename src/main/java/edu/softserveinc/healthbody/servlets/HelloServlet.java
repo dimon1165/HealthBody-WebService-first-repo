7333c463d03102fc99dc8a514a251ab39a657d39
Anatolii Tsaruk <anatolii.tsaruk@gmail.com>
2016-07-11 20:43:50 +0300

    Jenkins tesing 2

--- src/main/java/edu/softserveinc/healthbody/servlets/HelloServlet.java
+++ src/main/java/edu/softserveinc/healthbody/servlets/HelloServlet.java
@@ -27,7 +27,7 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Github push test 001. GET received by HelloServlet: ").append(request.getContextPath());
+		response.getWriter().append("Github push test 002. GET received by HelloServlet: ").append(request.getContextPath());
 	}
 
 	/**
@@ -35,7 +35,7 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Github push test 001. POST received by HelloServlet: ").append(request.getContextPath());
+		response.getWriter().append("Github push test 002. POST received by HelloServlet: ").append(request.getContextPath());
 	}
 
 }
