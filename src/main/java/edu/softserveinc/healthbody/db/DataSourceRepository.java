2c27f7733da2a40db37093ede6d334b8e1efe9ba
volodya4u <volodya4u@gmail.com>
2016-07-28 06:17:46 +0300

    Create parameters that handle which database.properties file we need to add

--- src/main/java/edu/softserveinc/healthbody/db/DataSourceRepository.java
+++ src/main/java/edu/softserveinc/healthbody/db/DataSourceRepository.java
@@ -24,42 +24,42 @@ public final class DataSourceRepository {
     public DataSource getPostgresOpenShift() throws JDBCDriverException {
         return new DataSource(DriverRepository.getInstance().getPostgresDriver(), "jdbc:postgresql://"
                 + System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST") + ":" + System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT")
-                + "/ws", ReadDBPropertyFile.DB_USERNAME_OPENSHIFT, ReadDBPropertyFile.DB_PASSWORD_OPENSHIFT);
+                + "/ws", ReadDBPropertyFile.DB_USERNAME, ReadDBPropertyFile.DB_PASSWORD);
     }
 
     public DataSource getPostgresJenkins() throws JDBCDriverException {
         return new DataSource(DriverRepository.getInstance().getPostgresDriver(),
         		"jdbc:postgresql://"
                         + System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST") + ":" + System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT")
-                        + "/jenkins", ReadDBPropertyFile.DB_USERNAME_JENKINS, ReadDBPropertyFile.DB_PASSWORD_JENKINS);
+                        + "/jenkins", ReadDBPropertyFile.DB_USERNAME, ReadDBPropertyFile.DB_PASSWORD);
     }
 
     public DataSource getPostgresJenkinsByDatabaseName(final String databaseName) throws JDBCDriverException {
         return new DataSource(DriverRepository.getInstance().getPostgresDriver(),
         		"jdbc:postgresql://"
                         + System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST") + ":" + System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT")
-                        + "/"+databaseName, ReadDBPropertyFile.DB_USERNAME_JENKINS, ReadDBPropertyFile.DB_PASSWORD_JENKINS);
+                        + "/"+databaseName, ReadDBPropertyFile.DB_USERNAME, ReadDBPropertyFile.DB_PASSWORD);
                 
     }
 
     public DataSource getPostgresLocalHost() throws JDBCDriverException {
         return new DataSource(DriverRepository.getInstance().getPostgresDriver(),
-        		ReadDBPropertyFile.DB_LOCALHOST_URL +"healthbodydb", ReadDBPropertyFile.DB_LOCALHOST_USERNAME , ReadDBPropertyFile.DB_LOCALHOST_PASSWORD);
+        		ReadDBPropertyFile.DB_URL +"healthbodydb", ReadDBPropertyFile.DB_USERNAME , ReadDBPropertyFile.DB_PASSWORD);
     }
 
     public DataSource getPostgresLocalHostTest() throws JDBCDriverException {
         return new DataSource(DriverRepository.getInstance().getPostgresDriver(),
-                ReadDBPropertyFile.DB_LOCALHOST_URL +"healthbodydbtest", ReadDBPropertyFile.DB_LOCALHOST_USERNAME , ReadDBPropertyFile.DB_LOCALHOST_PASSWORD);
+                ReadDBPropertyFile.DB_URL +"healthbodydbtest", ReadDBPropertyFile.DB_USERNAME , ReadDBPropertyFile.DB_PASSWORD);
     }
 
     public DataSource getPostgresLocalHostNoDatabase() throws JDBCDriverException {
-        return new DataSource(DriverRepository.getInstance().getPostgresDriver(), ReadDBPropertyFile.DB_LOCALHOST_URL,
-        		ReadDBPropertyFile.DB_LOCALHOST_USERNAME , ReadDBPropertyFile.DB_LOCALHOST_PASSWORD);
+        return new DataSource(DriverRepository.getInstance().getPostgresDriver(), ReadDBPropertyFile.DB_URL,
+        		ReadDBPropertyFile.DB_USERNAME , ReadDBPropertyFile.DB_PASSWORD);
     }
 
     public DataSource getPostgresLocalHostByDatabaseName(final String databaseName) throws JDBCDriverException {
-        return new DataSource(DriverRepository.getInstance().getPostgresDriver(), ReadDBPropertyFile.DB_LOCALHOST_URL
-                + databaseName, ReadDBPropertyFile.DB_LOCALHOST_USERNAME , ReadDBPropertyFile.DB_LOCALHOST_PASSWORD);
+        return new DataSource(DriverRepository.getInstance().getPostgresDriver(), ReadDBPropertyFile.DB_URL
+                + databaseName, ReadDBPropertyFile.DB_USERNAME , ReadDBPropertyFile.DB_PASSWORD);
     }
 
 }
--- src/main/java/edu/softserveinc/healthbody/db/ReadDBPropertyFile.java
+++ src/main/java/edu/softserveinc/healthbody/db/ReadDBPropertyFile.java
@@ -8,13 +8,9 @@ import edu.softserveinc.healthbody.log.Log4jWrapper;
 
 public class ReadDBPropertyFile {
 	
-	public static String DB_LOCALHOST_URL;
-	public static String DB_LOCALHOST_USERNAME;
-	public static String DB_LOCALHOST_PASSWORD;
-	public static String DB_USERNAME_JENKINS;
-	public static String DB_PASSWORD_JENKINS;
-	public static String DB_USERNAME_OPENSHIFT;
-	public static String DB_PASSWORD_OPENSHIFT;
+	public static String DB_URL;
+	public static String DB_USERNAME;
+	public static String DB_PASSWORD;
 	static {
 		try{
 
@@ -24,19 +20,9 @@ public class ReadDBPropertyFile {
 			properties.load(input);
 			input.close();
 			
-			//LocalHost DB			
-			DB_LOCALHOST_URL = properties.getProperty("DB_LOCALHOST_URL");
-			DB_LOCALHOST_USERNAME = properties.getProperty("DB_LOCALHOST_USERNAME");
-			DB_LOCALHOST_PASSWORD = properties.getProperty("DB_LOCALHOST_PASSWORD");
-			
-			//Jenkins DB
-			DB_USERNAME_JENKINS = properties.getProperty("DB_USERNAME_JENKINS");
-			DB_PASSWORD_JENKINS = properties.getProperty("DB_PASSWORD_JENKINS");
-		
-			//Openshift DB
-			DB_USERNAME_OPENSHIFT = properties.getProperty("DB_USERNAME_OPENSHIFT");
-			DB_PASSWORD_OPENSHIFT = properties.getProperty("DB_PASSWORD_OPENSHIFT");
-			
+			DB_URL = properties.getProperty("DB_URL");
+			DB_USERNAME = properties.getProperty("DB_USERNAME");
+			DB_PASSWORD = properties.getProperty("DB_PASSWORD");
 			
 		} catch (IOException ex) {
 			Log4jWrapper.get().info("Properties file is not found...");
