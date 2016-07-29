5694ce4183fda797244fa5bee10adb235eac6eff
volodya4u <volodya4u@gmail.com>
2016-07-29 12:24:31 +0300

    All mistakes to run UsersViewServiceImplTests corrected

--- src/test/java/edu/softserveinc/healthbody/users/UsersViewServiceImplTest.java
+++ src/test/java/edu/softserveinc/healthbody/users/UsersViewServiceImplTest.java
@@ -35,87 +35,82 @@ public class UsersViewServiceImplTest {
 
 	@Test
 	public void testUserViewGetAll() {
-		// TODO Fix test according to GUID implementation		
-//		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-//		List<UserDTO> ud1;
-//		try {
-//			ud1 = uvs.getAll(1, 2);
-//			assertNotNull(ud1);
-//			assertEquals(ud1.size(), 2);
-//			Log4jWrapper.get().info("testUserViewGetAll");
-//			Log4jWrapper.get().info(ud1.toString());
-//		} catch (JDBCDriverException | SQLException | TransactionException e) {
-//			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
-//			fail(TestConstants.EXCEPTION_CATCHED, e);
-//		}
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		List<UserDTO> ud1;
+		try {
+			ud1 = uvs.getAll(1, 2);
+			assertNotNull(ud1);
+			assertEquals(ud1.size(), 2);
+			Log4jWrapper.get().info("testUserViewGetAll");
+			Log4jWrapper.get().info(ud1.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
+			fail(TestConstants.EXCEPTION_CATCHED, e);
+		}
 	}
 
 	@Test 
-	public void testUserViewGetAllbyAdmin() {
-		// TODO Fix test according to GUID implementation		
-//		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-//		List<UserDTO> ud3;
-//		try {
-//			ud3 = uvs.getAllbyAdmin(1, 2);
-//			assertNotNull(ud3);
-//			assertEquals(ud3.size(), 2);
-//			Log4jWrapper.get().info("testUserViewGetAllbyAdmin");
-//			Log4jWrapper.get().info(ud3.toString());
-//		} catch (JDBCDriverException | SQLException | TransactionException e) {
-//			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
-//			fail(TestConstants.EXCEPTION_CATCHED, e);
-//		}
+	public void testUserViewGetAllbyAdmin() {	
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		List<UserDTO> ud3;
+		try {
+			ud3 = uvs.getAllbyAdmin(1, 2);
+			assertNotNull(ud3);
+			assertEquals(ud3.size(), 2);
+			Log4jWrapper.get().info("testUserViewGetAllbyAdmin");
+			Log4jWrapper.get().info(ud3.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
+			fail(TestConstants.EXCEPTION_CATCHED, e);
+		}
 	}
 
 	@Test
-	public void testUserViewGetAllinCompetition() {
-		// TODO Fix test according to GUID implementation		
-//		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-//		List<UserDTO> ud4;
-//		try {
-//			ud4 = uvs.getAllinCompetition(1, 2);
-//			assertNotNull(ud4);
-//			assertEquals(ud4.size(), 2);
-//			Log4jWrapper.get().info("testUserViewGetAllinCompetition");
-//			Log4jWrapper.get().info(ud4.toString());
-//		} catch (JDBCDriverException | SQLException | TransactionException e) {
-//			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
-//			fail(TestConstants.EXCEPTION_CATCHED, e);
-//		}
+	public void testUserViewGetAllinCompetition() {		
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		List<UserDTO> ud4;
+		try {
+			ud4 = uvs.getAllinCompetition(1, 2);
+			assertNotNull(ud4);
+			assertEquals(ud4.size(), 2);
+			Log4jWrapper.get().info("testUserViewGetAllinCompetition");
+			Log4jWrapper.get().info(ud4.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
+			fail(TestConstants.EXCEPTION_CATCHED, e);
+		}
 	}
 
 	@Test
-	public void testUserViewGetAllinGroup() {
-		// TODO Fix test according to GUID implementation		
-//		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-//		List<UserDTO> ud5;
-//		try {
-//			ud5 = uvs.getAllinGroup(1, 2);
-//			assertNotNull(ud5);
-//			assertEquals(ud5.size(), 2);
-//			Log4jWrapper.get().info("testUserViewGetAllinGroup");
-//			Log4jWrapper.get().info(ud5.toString());
-//		} catch (JDBCDriverException | SQLException | TransactionException e) {
-//			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
-//			fail(TestConstants.EXCEPTION_CATCHED, e);
-//		}
+	public void testUserViewGetAllinGroup() {		
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		List<UserDTO> ud5;
+		try {
+			ud5 = uvs.getAllinGroup(1, 2);
+			assertNotNull(ud5);
+			assertEquals(ud5.size(), 2);
+			Log4jWrapper.get().info("testUserViewGetAllinGroup");
+			Log4jWrapper.get().info(ud5.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
+			fail(TestConstants.EXCEPTION_CATCHED, e);
+		}
 	}
 
 	@Test
-	public void testUserViewGetAlltoAddInCompetition() {
-		// TODO Fix test according to GUID implementation		
-//		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
-//		List<UserDTO> ud6;
-//		try {
-//			ud6 = uvs.getAlltoAddInCompetition(1, 2);
-//			assertNotNull(ud6);
-//			assertEquals(ud6.size(), 2);
-//			Log4jWrapper.get().info("testUserViewGetAlltoAddInCompetition");
-//			Log4jWrapper.get().info(ud6.toString());
-//		} catch (JDBCDriverException | SQLException | TransactionException e) {
-//			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
-//			fail(TestConstants.EXCEPTION_CATCHED, e);
-//		}
+	public void testUserViewGetAlltoAddInCompetition() {		
+		UsersViewServiceImpl uvs = new UsersViewServiceImpl();
+		List<UserDTO> ud6;
+		try {
+			ud6 = uvs.getAlltoAddInCompetition(1, 2);
+			assertNotNull(ud6);
+			assertEquals(ud6.size(), 2);
+			Log4jWrapper.get().info("testUserViewGetAlltoAddInCompetition");
+			Log4jWrapper.get().info(ud6.toString());
+		} catch (JDBCDriverException | SQLException | TransactionException e) {
+			Log4jWrapper.get().error(TestConstants.EXCEPTION_CATCHED + e);
+			fail(TestConstants.EXCEPTION_CATCHED, e);
+		}
 	}
 
 
