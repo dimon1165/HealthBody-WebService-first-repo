f13475842d41da38c2025eb1f61dbf730520cebd
Anatolii Tsaruk <anatolii.tsaruk@gmail.com>
2016-08-01 03:03:02 +0300

    debug mode uncommented

--- src/main/java/edu/softserveinc/healthbody/log/Log4jWrapper.java
+++ src/main/java/edu/softserveinc/healthbody/log/Log4jWrapper.java
@@ -4,30 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class Log4jWrapper implements ILogger {
-//	private static volatile Log4jWrapper instance;
 	private final String PICTURE_PATH = "Path to Picture ";
 	private Logger logger;
 
-	// private Log4jWrapper() {
-	// this.logger = LoggerFactory.getLogger(Log4jWrapper.class);
-	// }
-	//
-	// public static Log4jWrapper get() {
-	// if (instance == null) {
-	// synchronized (Log4jWrapper.class) {
-	// if (instance == null) {
-	// instance = new Log4jWrapper();
-	// }
-	// }
-	// }
-	// return instance;
-	// }
-	
 	public static Logger get() {
-	    final Throwable t = new Throwable();
-	    final StackTraceElement methodCaller = t.getStackTrace()[1];
-	    final Logger logger = LoggerFactory.getLogger(methodCaller.getClassName());
-	    return logger;
+	    return LoggerFactory.getLogger(new Throwable().getStackTrace()[1].getClassName());
 	}
 
 	@Override
@@ -45,9 +26,10 @@ public final class Log4jWrapper implements ILogger {
 		logger.info(message);
 	}
 
-	// public void debug(String message) {
-	// logger.debug(message);
-	// }
+	@Override
+	public void debug(String message) {
+		logger.debug(message);
+	}
 
 	@Override
 	public void insertScreenShot(final String fileNamePath) {
